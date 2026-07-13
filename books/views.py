4d1f5565e2a394532db424463ca5a95dc2d9cd44
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-07-13 14:45:35 +0100

    Proper Pagination

--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import generics, status, permissions
 from .models import Book
 from .serializers import BookSerializer
+from .pagination import BookPagination
 
 
 
@@ -58,11 +59,13 @@ class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = BookPagination
     
     
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer    
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = BookPagination
     
     
