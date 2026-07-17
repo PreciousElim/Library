028267bb54aa39408092584c50ba1422cfab965b
Precious Elim <preciouselimhingbovo@gmail.comhiigit config --global user.email preciouselimhingbovo@gmail.com>
2026-07-17 16:32:56 +0100

    Loan app development

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from books.models import Book
+from django.conf import settings
 
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete = models.CASCADE, related_name ='loans')
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="loans")
+    borrowed_date = models.DateTimeField(auto_now_add=True)
+    returned_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.lender.username}"
+    
 # Create your models here.
